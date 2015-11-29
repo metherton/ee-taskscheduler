d5ce71c96cacb37ed7ae884cc5930f82181ad33b
metherton <martinetherton63@googlemail.com>
2015-11-29 20:04:32 +0100

    Use annotations instead of xml

--- src/main/java/com/martinetherton/AlarmClockInvoker.java
+++ src/main/java/com/martinetherton/AlarmClockInvoker.java
@@ -1,10 +1,13 @@
 package com.martinetherton;
 
+import org.springframework.scheduling.annotation.Scheduled;
+
 /**
  * Created by martin on 26/11/15.
  */
 public class AlarmClockInvoker {
 
+    @Scheduled(fixedDelay=5000)
     public void invoke() {
         System.out.println("Invoker invoked");
     }
